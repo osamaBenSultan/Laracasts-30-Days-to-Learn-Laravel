e47ab3e641a1b91360f53733b4d48ed87bb0aa04
Osama Sultan <80481444+osamaBenSultan@users.noreply.github.com>
2026-01-25 21:17:35 +0300

    from Day 1 to Day 10

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Job;
+use Illuminate\Support\Facades\Route;
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/jobs', function () {
+    $jobs = Job::all();
+    return view('jobs', compact('jobs'));
+});
+
+
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+
+    return view('job', ['job' => $job]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+
+
