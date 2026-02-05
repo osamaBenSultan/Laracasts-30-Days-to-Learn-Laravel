311e10cc4bd33758f131b5ecae9d13e70997764c
Osama Sultan <80481444+osamaBenSultan@users.noreply.github.com>
2026-02-05 11:52:54 +0300

    Make a Login and Registration System From Scratch: Part 1

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,44 @@
+<x-layout>
+
+    <x-slot:heading>
+        Login
+    </x-slot:heading>
+
+    <form method="POST" action="/login">
+        @csrf
+
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+
+                <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
+
+                    <x-form-field>
+                        <x-form-label for="email">Email</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input type="email" name="email" id="email"  placeholder="example@gmailcom" required></x-form-input>
+                        </div>
+                        <x-form-error name="email"></x-form-error>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for="password">Password</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input type="password" name="password" id="password"   required></x-form-input>
+                        </div>
+                        <x-form-error name="password"></x-form-error>
+                    </x-form-field>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+            <x-form-button >Log In</x-form-button>
+        </div>
+    </form>
+
+
+
+</x-layout>
